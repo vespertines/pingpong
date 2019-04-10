2e567540d723ae6c38c4daabc7507d7fcceccc5f
Kwang-Su Kim <atelier.jullian@gmail.com>
2019-04-10 05:15:14 +0100

    update dotenv setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,4 @@
-let contentfulConfig
-
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require("./.contentful")
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken:
-    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
-
-const { spaceId, accessToken } = contentfulConfig
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    "Contentful spaceId and the delivery token need to be provided."
-  )
-}
+require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
@@ -51,7 +31,10 @@ module.exports = {
     },
     {
       resolve: `gatsby-source-contentful`,
-      options: contentfulConfig,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || "",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
