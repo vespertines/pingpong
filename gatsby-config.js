b8c2939e886d6c7ce6cda81a8524ae120c5f3c40
Kwang-Su Kim <atelier.jullian@gmail.com>
2019-04-11 04:14:25 +0100

    update dotenv settings on gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,6 @@
-require("dotenv").config()
+require("dotenv").config({
+  path: '.env.${process.env.NODE_ENV}',
+})
 
 module.exports = {
   siteMetadata: {
@@ -32,8 +34,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID || "",
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
