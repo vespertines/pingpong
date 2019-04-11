bcabc2f1bd279decaea10ee3c209b36245a573a7
Kwang-Su Kim <atelier.jullian@gmail.com>
2019-04-11 04:19:34 +0100

    page updating and deploying test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hello World</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
